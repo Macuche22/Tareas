c18cba64340e2012d715ccd9d75e5919ba7a0616
Jonathan <60702311+Macuche22@users.noreply.github.com>
2020-02-06 09:27:33 -0600

    Add files via upload

---
+++ 20002742.js
@@ -0,0 +1,50 @@
+var mes = 10;
+switch(mes){
+  case 1:
+    alert("es invierno");
+    break;
+  case 2:
+    alert("es invierno");
+    break;
+  case 3:
+    alert("es primavera");
+    break;
+  case 4:
+    alert("es primavera");
+    break;
+  case 5:
+    alert("es primavera");
+    break;
+  case 6:
+    alert("es verano");
+    break;
+  case 7:
+    alert("es verano");
+    break;
+  case 8:
+    alert("es verano");
+    break;
+  case 9:
+    alert("es otoño");
+    break;
+  case 10:
+    alert("es otoño");
+    break;
+  case 11:
+    alert("es otoño");
+    break;
+  case 12:
+    alert("es invierno");
+    break;
+    
+}
+var mes = prompt("ingrese algun dato de 1 a 12");
+if(mes == 12){
+  alert("es invierno");
+} else if(mes == 2){
+alert("es invierno");
+} else {
+  alert("pertenece a otra estacion");
+}
+
+    alert("20002742 Jonathan Manuel Medina Castellanos");
